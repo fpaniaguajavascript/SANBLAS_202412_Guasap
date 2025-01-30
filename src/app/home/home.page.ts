e5c535a67cbd38490c978662d3598e61e31c41dd
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2025-01-30 13:09:16 +0100

    Funcionamiento básico

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,23 @@
 import { Component, inject } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonItem, IonList, IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { FirebaseService } from '../services/firebase.service';
+import { FormsModule } from '@angular/forms';
+import { Mensaje } from '../interfaces/mensaje';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonItem, IonList, IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, FormsModule],
 })
 export class HomePage {
+  mensaje : Mensaje = {};
   firebaseService = inject(FirebaseService);
+  
   constructor() {
-    
+  }
+  enviar(){
+    this.firebaseService.enviarMensaje(this.mensaje);
+    this.mensaje.mensaje = "";
   }
 }
